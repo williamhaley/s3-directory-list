390bf2eb5e64a4fc9fb4c083c770b0c643057587
Will <willhy@gmail.com>
2017-10-18 22:03:00 -0500

    Query based on current bucket. Root will show all.

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const listObjects = ((config = {}) => {
 	// `bucket` required for vanity URLs/DNS when we cannot infer bucket name.
 	settings.bucketName === null && assert('Bucket name must be specified!');
 
-	const template = $('#template-s3object').html();
-	const render   = Handlebars.compile(template);
-	const query    = 'list-type=2';
-	const restURL  = `http://${settings.bucketName}.s3.amazonaws.com/?${query}`;
+	const template   = $('#template-s3object').html();
+	const render     = Handlebars.compile(template);
+	const pathPrefix = (location.pathname || '').substr(1);
+	const query      = `list-type=2&prefix=${pathPrefix}`;
+	const restURL    = `http://${settings.bucketName}.s3.amazonaws.com/?${query}`;
 
 	// Call the API and update the view with the parsed results.
 	$.get(restURL).done((res) => {
@@ -24,7 +25,7 @@ const listObjects = ((config = {}) => {
 
 		$(xmlDoc).find('Contents').toArray().map((element) => {
 			const $element = $(element);
-			const key      = $element.find('Key').text();
+			const key      = $element.find('Key').text().match(/(.*\/)?(.*)/)[2];
 
 			if (settings.excludedObjects.includes(key)) {
 				return;
